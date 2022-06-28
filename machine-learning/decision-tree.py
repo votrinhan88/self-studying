f527c2aec82a050a18cf1f45e69502eaeb60ebbe
votrinhan88 <votrinhan88@gmail.com>
2022-06-28 07:58:48 +0700

    Fix branching even with unique distr; Add Node's __repr__();

--- machine-learning/decision-tree.py
+++ machine-learning/decision-tree.py
@@ -4,6 +4,8 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import pandas as pd
+# from typing import List
+
 
 # Load and process data
 data = pd.read_csv('./data/iris.csv')
@@ -33,9 +35,14 @@ class Node():
 
         self.distr     = None
 
+    # Magic method to represent variable
+    def __repr__(self) -> str:
+        # Magic attribute to get class name
+        return f"{self.__class__.__name__} {self.path}, I = {self.info}, distr = {self.distr.numpy()}"
+
 class DecisionTreeClassier():
-    def __init__(self, num_splits, method_info = 'entropy'):
-        self.num_splits = num_splits
+    def __init__(self, max_depth = 4, method_info = 'entropy'):
+        self.max_depth = max_depth
         self.method_info = method_info
         self.depth = 0
 
@@ -46,16 +53,11 @@ class DecisionTreeClassier():
         self.num_classes = y_train.unique().size()[0]
 
         self.stump = Node(depth = 0)
+        self.stump.distr = F.one_hot(y_train.squeeze(dim = 1), num_classes = self.num_classes).sum(dim = 0)
+        self.stump.info = self.compute_info(y_train)
         self.grow(node = self.stump, X_node = X_train, y_node = y_train)
 
     def grow(self, node:Node, X_node:torch.Tensor, y_node:torch.Tensor):
-        # Check if node is leaf
-        node.distr = F.one_hot(y_node.squeeze(), num_classes = self.num_classes).sum(dim = 0)
-        node.info = self.compute_info(y_node)
-        if len(y_node.unique()) == 1:
-            node.is_leaf = True
-            return
-        
         max_gain = self.find_best_split(node, X_node, y_node)
         if max_gain > 0:
             # Continue growing
@@ -70,10 +72,16 @@ class DecisionTreeClassier():
             X_left, y_left = X_node[left_ind], y_node[left_ind]
             X_right, y_right = X_node[~left_ind], y_node[~left_ind]
             
-            # Check if each branch is already leaf
-            # Arrive at leaf node
-            self.grow(left_node, X_left, y_left)
-            self.grow(right_node, X_right, y_right)        
+            # Leaf node:
+            #  - Has a unique class distribution (contains only one class)
+            #  - OR has reached max depth
+            for (branch, X_branch, y_branch) in zip([left_node, right_node], [X_left, X_right], [y_left, y_right]):
+                branch.distr = F.one_hot(y_branch.squeeze(dim = 1), num_classes = self.num_classes).sum(dim = 0)
+                branch.info = self.compute_info(y_branch)
+                if (len(y_branch.unique()) == 1) | (branch.depth == self.max_depth):
+                    branch.is_leaf = True
+                else:
+                    self.grow(branch, X_branch, y_branch)
     
     def find_best_split(self, node:Node, X_node:torch.Tensor, y_node:torch.Tensor):
         max_gain = -torch.tensor(float('inf'))
@@ -91,7 +99,7 @@ class DecisionTreeClassier():
                     opt_feature = feature
                     opt_threshold = threshold
         
-        # Update node
+        # Update node with optimal split
         node.feature = opt_feature
         node.threshold = opt_threshold
         
@@ -121,16 +129,18 @@ class DecisionTreeClassier():
             if node.is_leaf == True:
                 print(f"{'    '*node.depth} Branch {node.path}: {node.distr.numpy()}")
             elif node.is_leaf == False:
-                print(f"{'    '*node.depth} Branch {node.path} (x{node.feature.item()}, {node.threshold.item():.2f}): {node.distr.numpy()}")
+                if node.depth == 0:
+                    print(f"{'    '*node.depth} Stump (x{node.feature.item()}, {node.threshold.item():.2f}): {node.distr.numpy()}")
+                elif node.depth > 0:
+                    print(f"{'    '*node.depth} Branch {node.path} (x{node.feature.item()}, {node.threshold.item():.2f}): {node.distr.numpy()}")
                 for branch in node.children:
                     traverse_print(branch)
             
         traverse_print(self.stump)
 
 
-h = DecisionTreeClassier(num_splits = 50, method_info = 'Gini')
+h = DecisionTreeClassier(max_depth = 4, method_info = 'Gini')
 h.fit(X_train, y_train)
 h.print_tree()
-
 
 print()
