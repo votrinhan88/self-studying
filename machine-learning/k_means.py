394b8ee0b2b4724327c5ec5925fa3643c47fdb99
votrinhan88 <votrinhan88@gmail.com>
2022-07-02 15:02:26 +0700

    Move get_2D_clusters() into utils_data.py; Optimize K-means

--- machine-learning/k_means.py
+++ machine-learning/k_means.py
@@ -1,79 +1,58 @@
+# Topic: Unsupervised, Clustering
+
 import torch
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from utils_data import get_2D_clusters
 
 # Constants
-DATASET_SIZE = 600
 NUM_CLUSTERS = 4
-RADIUS = 1
-SIGMA_DIAG = 0.2
 K = 8
 COLORS = ['red', 'blue', 'green', 'magenta', 'cyan', 'yellow', 'black', 'tab:blue', 'tab:orange', 'tab:green',
           'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
 PLOT_STEP = 0.01
-pi = torch.acos(torch.zeros(1)).item()*2
-
-# Generate dummy data
-torch.manual_seed(17)
-# Mu and Sigma for Gaussian distributions
-mu = torch.cat(
-    [RADIUS*torch.cos(torch.arange(NUM_CLUSTERS)*2*pi/NUM_CLUSTERS).unsqueeze(dim = 1),
-     RADIUS*torch.sin(torch.arange(NUM_CLUSTERS)*2*pi/NUM_CLUSTERS).unsqueeze(dim = 1)],
-    dim = 1)
-sigma = SIGMA_DIAG*torch.tensor([[1., 0.], [0., 1.]])
-# Sample from Gaussian distributions
-examples_per_cluster = (torch.tensor(DATASET_SIZE)/NUM_CLUSTERS).type(torch.int)
-x_raw = torch.cat([torch.distributions.multivariate_normal.MultivariateNormal(mu[cluster, :], sigma).sample([examples_per_cluster]) for cluster in range(NUM_CLUSTERS)], dim = 0)
-y_raw = torch.cat([torch.tensor([[cluster]]*examples_per_cluster) for cluster in range(NUM_CLUSTERS)], dim = 0)
-# Shuffle data
-shuffle_index = torch.randperm(x_raw.size()[0])
-x_raw = x_raw[shuffle_index]
-y_raw = y_raw[shuffle_index]
-
 
 # K-means Algorithm
 class KMeansClassifier():
-    def __init__(self, K, train_data):
+    def __init__(self, K, X_train):
         self.K = K
-        self.centroids:torch.Tensor = train_data[torch.randperm(train_data.size()[0])][range(self.K)]
-
+        self.centroids:torch.Tensor = X_train[torch.randperm(X_train.size()[0])][range(self.K)]
+    
     # Fix centroids, update labels
-    def forward(self, input:torch.Tensor):
-        distance = torch.zeros([input.size()[0], self.K])
+    def forward(self, X_train:torch.Tensor):
+        distance = torch.zeros([X_train.size()[0], self.K])
         for k in torch.arange(self.K):
-            distance[:, k] = torch.sqrt(torch.sum((self.centroids[k, :] - input)**2, dim = 1))
+            distance[:, k] = torch.sqrt(torch.sum((self.centroids[k, :] - X_train)**2, dim = 1))
         
-        (_, yhat) = torch.min(distance, dim = 1)
-        yhat = yhat.unsqueeze(dim = 1)
-        
+        (_, yhat) = torch.min(distance, dim = 1, keepdim = True)        
         return yhat
 
     # Fix labels, update centroids
-    def backward(self, yhat:torch.Tensor):
+    def backward(self, X_train:torch.Tensor, yhat:torch.Tensor):
         for k in torch.arange(self.K):
-            self.centroids[k, :] = torch.mean(x_raw[yhat.squeeze() == k, :], dim = 0)
+            self.centroids[k, :] = torch.mean(X_train[yhat.squeeze() == k, :], dim = 0)
+
+X_train = get_2D_clusters(num_clusters=NUM_CLUSTERS)[0]
 
 # Init
-h = KMeansClassifier(K, x_raw)
+h = KMeansClassifier(K, X_train)
 centroids_log = h.centroids.clone().unsqueeze(dim = 0)
 # Training loop
 for epoch in torch.arange(20):
     # Fix centroids, update labels
-    yhat = h.forward(x_raw)
+    yhat = h.forward(X_train)
     # Fix labels, update centroids
-    h.backward(yhat)
-
+    h.backward(X_train, yhat)
     # Check for stopping condition: when centroids stop moving
     if (centroids_log[-1, :, :] == h.centroids).prod().bool() == True:
         print(f'Stopped at epoch {epoch.item()}! Centroids stop moving.')
         break
-    
     # Log centroids position
     centroids_log = torch.cat([centroids_log, h.centroids.unsqueeze(dim = 0)], dim = 0)
 
 # Plot all centroids and examples 
 fig, ax = plt.subplots()
-ax.set_title(f'{K}-means with {NUM_CLUSTERS} clusters')
+ax.set_title(f'K-means clustering (K = {K}) with {NUM_CLUSTERS} given clusters')
 for k in torch.arange(h.K):
     # Centroids' path
     ax.plot(centroids_log[:, k, 0], centroids_log[:, k, 1],
@@ -81,12 +60,13 @@ for k in torch.arange(h.K):
     ax.scatter(centroids_log[:, k, 0], centroids_log[:, k, 1],
                color = COLORS[k], marker = 'x')
     # Training data
-    ax.scatter(x_raw[yhat.squeeze() == k, 0], x_raw[yhat.squeeze() == k, 1],
+    ax.scatter(X_train[yhat.squeeze() == k, 0], X_train[yhat.squeeze() == k, 1],
                color = COLORS[k], alpha = 0.7, s = 10, zorder = 100)
 
 # Voronoi diagram for centroids
-plot_x1 = torch.arange(x_raw[:, 0].min() - 0.2*RADIUS, x_raw[:, 0].max() + 0.2*RADIUS, PLOT_STEP)
-plot_x2 = torch.arange(x_raw[:, 1].min() - 0.2*RADIUS, x_raw[:, 1].max() + 0.2*RADIUS, PLOT_STEP)
+ptp_X = X_train.max(dim = 0)[0] - X_train.min(dim = 0)[0]
+plot_x1 = torch.arange(X_train[:, 0].min() - 0.2*ptp_X[0], X_train[:, 0].max() + 0.2*ptp_X[1], PLOT_STEP)
+plot_x2 = torch.arange(X_train[:, 1].min() - 0.2*ptp_X[0], X_train[:, 1].max() + 0.2*ptp_X[1], PLOT_STEP)
 x1, x2 = torch.meshgrid([plot_x1 - PLOT_STEP/2, plot_x2 - PLOT_STEP/2])
 x = torch.cat([x1.flatten().unsqueeze(dim = 1), x2.flatten().unsqueeze(dim = 1)], dim = 1)
 plot_yhat = h.forward(x).reshape([plot_x1.size()[0], plot_x2.size()[0]])
--- machine-learning/utils_data.py
+++ machine-learning/utils_data.py
@@ -13,9 +13,22 @@ def get_iris():
     # Shuffle data, split to train and test set
     data = data.iloc[torch.randperm(data_size), :]
     X, y = (torch.tensor(data.iloc[:, 0:-1].to_numpy()),
-            torch.tensor(data.iloc[:, [-1]].to_numpy(dtype = np.int64)))
-    num_trains = round(data_size*0.8)
-    X_train, y_train = X[0:num_trains], y[0:num_trains]
-    X_test, y_test = X[num_trains:], y[num_trains:]
+        torch.tensor(data.iloc[:, [-1]].to_numpy(dtype = np.int64)))
+    return X, y
 
-    return X_train, y_train, X_test, y_test
+def get_2D_clusters(num_clusters, sigma_diag = 0.2, radius = 1, num_examples = 600):
+    pi = torch.acos(torch.zeros(1)).item()*2
+    # Mu and Sigma for Gaussian distributions
+    mu = torch.cat(
+        [radius*torch.cos(torch.arange(num_clusters)*2*pi/num_clusters).unsqueeze(dim = 1),
+         radius*torch.sin(torch.arange(num_clusters)*2*pi/num_clusters).unsqueeze(dim = 1)],
+        dim = 1)
+    sigma = sigma_diag*torch.tensor([[1., 0.], [0., 1.]])
+    # Sample from Gaussian distributions
+    examples_per_cluster = torch.tensor(num_examples/num_clusters, dtype = torch.int)
+    X = torch.cat([torch.distributions.multivariate_normal.MultivariateNormal(mu[cluster, :], sigma).sample([examples_per_cluster]) for cluster in range(num_clusters)], dim = 0)
+    y = torch.cat([torch.tensor([[cluster]]*examples_per_cluster) for cluster in range(num_clusters)], dim = 0)
+    # Shuffle data
+    shuffle_index = torch.randperm(X.size()[0])
+    X, y = X[shuffle_index], y[shuffle_index]
+    return X, y
