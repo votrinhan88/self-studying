689c59439fa64e9f74c7bc72f6c2349c0a845a03
votrinhan88 <votrinhan88@gmail.com>
2022-07-02 18:58:18 +0700

    Decision Tree: move code into main + add regressor (to be merged later)

--- machine-learning/decision_tree_regressor.py
+++ machine-learning/decision_tree_regressor.py
@@ -1,38 +1,36 @@
-# Topic: Supervised, Classification
-
 import torch
-import torch.nn.functional as F
+from typing import List
 
-class Node():
+class NodeRegressor():
     def __init__(self, depth = None, info = None, feature = None, threshold = None, parent = None,
                  children = [], path:str = ''):
         self.is_leaf = False
         # Pre-allocate
-        self.depth     = depth
-        self.info      = info
-        self.feature   = feature
-        self.threshold = threshold
-        self.parent    = parent
-        self.children  = children
-        self.path      = path
+        self.depth = depth
+        self.info:torch.Tensor            = info
+        self.feature:torch.Tensor         = feature
+        self.threshold:torch.Tensor       = threshold
+        self.parent:NodeRegressor         = parent
+        self.children:List[NodeRegressor] = children
+        self.path                         = path
 
-        self.distr:torch.Tensor = None
+        self.mean:torch.Tensor = None
 
     # Magic method to represent variable
     def __repr__(self) -> str:
         # Magic attribute to get class name
         if self.depth == 0:
-            return f"{self.__class__.__name__} Root, I = {self.info:.3f}, distr = {self.distr.numpy()}"
+            return f"{self.__class__.__name__} Root, I = {self.info:.3f}, mean = {self.mean.numpy()}"
         elif self.depth > 0:
-            return f"{self.__class__.__name__} {self.path}, I = {self.info:.3f}, distr = {self.distr.numpy()}"
+            return f"{self.__class__.__name__} {self.path}, I = {self.info:.3f}, mean = {self.mean.numpy()}"
 
     def branch(self):
-        left_node  = Node(depth = self.depth + 1, path = self.path + 'L', parent = self)
-        right_node = Node(depth = self.depth + 1, path = self.path + 'R', parent = self)
+        left_node  = NodeRegressor(depth = self.depth + 1, path = self.path + 'L', parent = self)
+        right_node = NodeRegressor(depth = self.depth + 1, path = self.path + 'R', parent = self)
         return left_node, right_node
 
-class DecisionTreeClassifier():
-    def __init__(self, max_depth = 4, method_info = 'Gini'):
+class DecisionTreeRegressor():
+    def __init__(self, max_depth = 4, method_info = 'var'):
         self.max_depth = max_depth
         self.method_info = method_info
         self.depth = 0
@@ -44,14 +42,13 @@ class DecisionTreeClassifier():
         self.X_train = X_train
         self.y_train = y_train
         self.num_features = X_train.size()[1]
-        self.num_classes = y_train.unique().size()[0]
 
-        self.root = Node(depth = 0)
-        self.root.distr = F.one_hot(y_train.squeeze(dim = 1), num_classes = self.num_classes).sum(dim = 0)
+        self.root = NodeRegressor(depth = 0)
+        self.root.mean = y_train.mean()
         self.root.info = self.compute_info(y_train)
         self.grow(node = self.root, X_node = X_train, y_node = y_train)
 
-    def grow(self, node:Node, X_node:torch.Tensor, y_node:torch.Tensor):
+    def grow(self, node:NodeRegressor, X_node:torch.Tensor, y_node:torch.Tensor):
         max_gain = self.find_best_split(node, X_node, y_node)
         if max_gain > 0:
             # Continue growing
@@ -67,14 +64,14 @@ class DecisionTreeClassifier():
             #  - Has a unique class distribution (contains only one class)
             #  - OR has reached max depth
             for (branch, X_branch, y_branch) in zip([left_node, right_node], [X_left, X_right], [y_left, y_right]):
-                branch.distr = F.one_hot(y_branch.squeeze(dim = 1), num_classes = self.num_classes).sum(dim = 0)
+                branch.mean = y_branch.mean()
                 branch.info = self.compute_info(y_branch)
                 if (len(y_branch.unique()) == 1) | (branch.depth == self.max_depth):
                     branch.is_leaf = True
                 else:
                     self.grow(branch, X_branch, y_branch)
     
-    def find_best_split(self, node:Node, X_node:torch.Tensor, y_node:torch.Tensor):
+    def find_best_split(self, node:NodeRegressor, X_node:torch.Tensor, y_node:torch.Tensor):
         max_gain = -torch.tensor(float('inf'))
         for feature in torch.arange(self.num_features):
             # thresholds = torch.linspace(start = X_node[:, feature].min(),
@@ -83,8 +80,8 @@ class DecisionTreeClassifier():
             uniques = X_node[:, feature].sort()[0].unique()
             thresholds = (uniques[1:] + uniques[:-1])/2
             for threshold in thresholds:
-                left, right = self.split(X_node, y_node, feature, threshold)
-                gain = self.compute_gain(node, y_node, left, right)
+                y_left, y_right = self.split(X_node, y_node, feature, threshold)
+                gain = self.compute_gain(node, y_node, y_left, y_right)
                 if gain > max_gain:
                     max_gain = gain
                     opt_feature = feature
@@ -99,37 +96,29 @@ class DecisionTreeClassifier():
         right_ys = y_node[X_node[:, feature] >= threshold]
         return left_ys, right_ys
 
-    def compute_gain(self, node:Node, y_node:torch.Tensor, left:torch.Tensor, right:torch.Tensor):
+    def compute_gain(self, node:NodeRegressor, y_node:torch.Tensor, left:torch.Tensor, right:torch.Tensor):
         left_info = self.compute_info(left)
         right_info = self.compute_info(right)
         gain = node.info - (left.size()[0]*left_info + right.size()[0]*right_info)/y_node.size()[0]
         return gain
 
     def compute_info(self, label:torch.Tensor) -> torch.Tensor:
-        distr = F.one_hot(label.squeeze(dim = 1), num_classes = self.num_classes).sum(dim = 0)
-        distr = distr/distr.sum()
-        if self.method_info == 'Gini':
-            info = 1 - (distr**2).sum()
-        elif self.method_info == 'entropy':
-            # Side note:
-            # 1. Entropy of a system (all classes) = sum of entropy of its parts (each class)
-            # 2. Moreover, if a class has no examples, it is absolutely predictable absent, hence its
-            #   entropy is 0.
-            # 3. To ease dealing with absent classes (which yields log(0) = -inf), and (2.), the 
-            #   computation only considers existing classes.
-            info = -(distr[distr != 0]*distr[distr != 0].log()).sum()
+        if self.method_info == 'var':
+            info = ((label - label.mean())**2).sum()/label.size()[0]
+        elif self.method_info == 'std':
+            info = ((label - label.mean())**2).sum().sqrt()/label.size()[0]
         return info
 
     def print_tree(self):
-        def traverse_print(node: Node):
+        def traverse_print(node:NodeRegressor):
             # Print node
             if node.depth == 0:
-                print(f"Root: {node.distr.numpy()}")
+                print(f"Root: {node.mean.item()}")
             elif node.depth > 0:
                 if node.path[-1] == 'L':
-                    print(f"{'    '*node.depth}Branch {node.path} (x{node.parent.feature.item()} ≤ {node.parent.threshold.item():.2f}): {node.distr.numpy()}")
+                    print(f"{'    '*node.depth}Branch {node.path} (x{node.parent.feature.item()} ≤ {node.parent.threshold.item():.2f}): {node.mean.item()}")
                 if node.path[-1] == 'R':
-                    print(f"{'    '*node.depth}Branch {node.path} (x{node.parent.feature.item()} > {node.parent.threshold.item():.2f}): {node.distr.numpy()}")
+                    print(f"{'    '*node.depth}Branch {node.path} (x{node.parent.feature.item()} > {node.parent.threshold.item():.2f}): {node.mean.item()}")
             # Go to children branches
             if node.is_leaf == False:
                 for branch in node.children:
@@ -140,12 +129,12 @@ class DecisionTreeClassifier():
     def forward(self, input:torch.Tensor, method = 'all') -> torch.Tensor:
         if method == 'each':
             # Method 1: Loop and traverse each example through the tree
-            def traverse_forward(node:Node, input:torch.Tensor, yhat) -> torch.Tensor:
+            def traverse_forward(node:NodeRegressor, input:torch.Tensor, yhat) -> torch.Tensor:
                 if yhat is not None:
                     return yhat
                 elif yhat is None:
                     if node.is_leaf == True:
-                        return node.distr.max(dim = 0)[1]
+                        return node.mean
                     elif node.is_leaf == False:
                         if input[node.feature] < node.threshold:
                             return traverse_forward(node.children[0], input, yhat)
@@ -159,9 +148,9 @@ class DecisionTreeClassifier():
 
         elif method == 'all':
             # Method 2: Traverse all examples through the tree at once
-            def traverse_forward(node:Node, input:torch.Tensor, yhat:torch.Tensor, yhat_id:torch.Tensor) -> torch.Tensor:
+            def traverse_forward(node:NodeRegressor, input:torch.Tensor, yhat:torch.Tensor, yhat_id:torch.Tensor) -> torch.Tensor:
                 if node.is_leaf == True:
-                    yhat[yhat_id.squeeze()] = node.distr.max(dim = 0)[1]
+                    yhat[yhat_id.squeeze()] = node.mean
                     return yhat
                 elif node.is_leaf == False:
                     left_ind = input[:, node.feature] < node.threshold
@@ -172,43 +161,30 @@ class DecisionTreeClassifier():
                             yhat = traverse_forward(branch, branch_input, yhat, branch_yhat_id)
                     return yhat
 
-            yhat = -torch.ones([input.size()[0], 1], dtype = torch.long)
+            yhat = torch.zeros([input.size()[0], 1])
             yhat_id = torch.arange(input.size()[0]).unsqueeze(dim = 1)
             yhat = traverse_forward(self.root, input, yhat, yhat_id)
             return yhat
 
-if __name__ == '__main__':
-    from utils_data import get_iris
+# Dummy data
+X_train = 10*torch.rand([20, 1])
+y_train = 0.8*X_train + torch.sin(X_train)
 
-    X, y = get_iris()
-    num_trains = round(X.size()[0]*0.8)
-    X_train, y_train = X[0:num_trains], y[0:num_trains]
-    X_test, y_test = X[num_trains:], y[num_trains:]
+h = DecisionTreeRegressor(max_depth = 4)
+h.fit(X_train, y_train)
 
-    h = DecisionTreeClassifier(max_depth = 2)
-    h.fit(X_train, y_train)
-    h.print_tree()
-    yhat = h.forward(X_test)
-    print(f'Accuracy = {((yhat == y_test).sum()/y_test.size()[0]).item():.4f}')
+X_test = torch.arange(start = -2, end = 12, step = 0.01).unsqueeze(dim = 1)
+y_test = h.forward(X_test)
 
 
-'''
-Snippet to benchmarking tree.forward() using method = 'each' vs 'all'
-
-import time
-
-start = time.time()
-yhat_each = h.forward(X_test, method = 'each')
-end = time.time()
-time1 = end - start
-print(f'Forwarding examples one-by-one: {time1:.2e} s')
-
-start = time.time()
-yhat_all = h.forward(X_test, method = 'all')
-end = time.time()
-time2 = end - start
-print(f'Forwarding examples all at once: {time2:.2e} s')
-
-print(f'Faster how many times? {time1/time2:.2f}')
-print(f'Confirm results of both methods are the same: {(yhat_each == yhat_all).prod().bool().item()}')
-'''
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(constrained_layout = True)
+ax.scatter(X_train, y_train,
+           color = 'black', s = 40, label = 'Observed')
+ax.step(X_test, y_test,
+        linewidth = 2, color = 'blue', label = 'DT prediction')
+ax.plot(X_test, 0.8*X_test + torch.sin(X_test),
+        linewidth = 1, linestyle = 'dashed', color = 'red', alpha = 0.5, label = 'Ground truth')
+ax.set(title = f"Decision Tree Regressor with depth {h.depth} (max {h.max_depth})")
+ax.legend()
+plt.show()
