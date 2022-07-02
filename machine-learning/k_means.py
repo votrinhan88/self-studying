9abba01f7c584ba5861dc1cf3be77ea167d8d541
votrinhan88 <votrinhan88@gmail.com>
2022-07-02 15:09:37 +0700

    K-means: Put code inside **if __name__ == '__main__'**

--- machine-learning/k_means.py
+++ machine-learning/k_means.py
@@ -1,16 +1,5 @@
 # Topic: Unsupervised, Clustering
-
 import torch
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from utils_data import get_2D_clusters
-
-# Constants
-NUM_CLUSTERS = 4
-K = 8
-COLORS = ['red', 'blue', 'green', 'magenta', 'cyan', 'yellow', 'black', 'tab:blue', 'tab:orange', 'tab:green',
-          'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
-PLOT_STEP = 0.01
 
 # K-means Algorithm
 class KMeansClassifier():
@@ -32,46 +21,56 @@ class KMeansClassifier():
         for k in torch.arange(self.K):
             self.centroids[k, :] = torch.mean(X_train[yhat.squeeze() == k, :], dim = 0)
 
-X_train = get_2D_clusters(num_clusters=NUM_CLUSTERS)[0]
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import ListedColormap
+    from utils_data import get_2D_clusters
 
-# Init
-h = KMeansClassifier(K, X_train)
-centroids_log = h.centroids.clone().unsqueeze(dim = 0)
-# Training loop
-for epoch in torch.arange(20):
-    # Fix centroids, update labels
-    yhat = h.forward(X_train)
-    # Fix labels, update centroids
-    h.backward(X_train, yhat)
-    # Check for stopping condition: when centroids stop moving
-    if (centroids_log[-1, :, :] == h.centroids).prod().bool() == True:
-        print(f'Stopped at epoch {epoch.item()}! Centroids stop moving.')
-        break
-    # Log centroids position
-    centroids_log = torch.cat([centroids_log, h.centroids.unsqueeze(dim = 0)], dim = 0)
+    NUM_CLUSTERS = 4
+    K = 8
+    COLORS = ['red', 'blue', 'green', 'magenta', 'cyan', 'yellow', 'black', 'tab:blue', 'tab:orange', 'tab:green',
+            'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+    PLOT_STEP = 0.01
 
-# Plot all centroids and examples 
-fig, ax = plt.subplots()
-ax.set_title(f'K-means clustering (K = {K}) with {NUM_CLUSTERS} given clusters')
-for k in torch.arange(h.K):
-    # Centroids' path
-    ax.plot(centroids_log[:, k, 0], centroids_log[:, k, 1],
-            color = COLORS[k], linestyle = 'dashed')
-    ax.scatter(centroids_log[:, k, 0], centroids_log[:, k, 1],
-               color = COLORS[k], marker = 'x')
-    # Training data
-    ax.scatter(X_train[yhat.squeeze() == k, 0], X_train[yhat.squeeze() == k, 1],
-               color = COLORS[k], alpha = 0.7, s = 10, zorder = 100)
+    X_train = get_2D_clusters(num_clusters=NUM_CLUSTERS)[0]
 
-# Voronoi diagram for centroids
-ptp_X = X_train.max(dim = 0)[0] - X_train.min(dim = 0)[0]
-plot_x1 = torch.arange(X_train[:, 0].min() - 0.2*ptp_X[0], X_train[:, 0].max() + 0.2*ptp_X[1], PLOT_STEP)
-plot_x2 = torch.arange(X_train[:, 1].min() - 0.2*ptp_X[0], X_train[:, 1].max() + 0.2*ptp_X[1], PLOT_STEP)
-x1, x2 = torch.meshgrid([plot_x1 - PLOT_STEP/2, plot_x2 - PLOT_STEP/2])
-x = torch.cat([x1.flatten().unsqueeze(dim = 1), x2.flatten().unsqueeze(dim = 1)], dim = 1)
-plot_yhat = h.forward(x).reshape([plot_x1.size()[0], plot_x2.size()[0]])
+    # Init
+    h = KMeansClassifier(K, X_train)
+    centroids_log = h.centroids.clone().unsqueeze(dim = 0)
+    # Training loop
+    for epoch in torch.arange(20):
+        # Fix centroids, update labels
+        yhat = h.forward(X_train)
+        # Fix labels, update centroids
+        h.backward(X_train, yhat)
+        # Check for stopping condition: when centroids stop moving
+        if (centroids_log[-1, :, :] == h.centroids).prod().bool() == True:
+            print(f'Stopped at epoch {epoch.item()}! Centroids stop moving.')
+            break
+        # Log centroids position
+        centroids_log = torch.cat([centroids_log, h.centroids.unsqueeze(dim = 0)], dim = 0)
 
-ax.pcolormesh(x1, x2, plot_yhat, cmap = ListedColormap(COLORS[0:h.K]), alpha = 0.5, shading = 'auto')
+    # Plot all centroids and examples 
+    fig, ax = plt.subplots()
+    ax.set_title(f'K-means clustering (K = {K}) with {NUM_CLUSTERS} given clusters')
+    for k in torch.arange(h.K):
+        # Centroids' path
+        ax.plot(centroids_log[:, k, 0], centroids_log[:, k, 1],
+                color = COLORS[k], linestyle = 'dashed')
+        ax.scatter(centroids_log[:, k, 0], centroids_log[:, k, 1],
+                color = COLORS[k], marker = 'x')
+        # Training data
+        ax.scatter(X_train[yhat.squeeze() == k, 0], X_train[yhat.squeeze() == k, 1],
+                color = COLORS[k], alpha = 0.7, s = 10, zorder = 100)
 
-plt.show()
-print()
+    # Voronoi diagram for centroids
+    ptp_X = X_train.max(dim = 0)[0] - X_train.min(dim = 0)[0]
+    plot_x1 = torch.arange(X_train[:, 0].min() - 0.2*ptp_X[0], X_train[:, 0].max() + 0.2*ptp_X[1], PLOT_STEP)
+    plot_x2 = torch.arange(X_train[:, 1].min() - 0.2*ptp_X[0], X_train[:, 1].max() + 0.2*ptp_X[1], PLOT_STEP)
+    x1, x2 = torch.meshgrid([plot_x1 - PLOT_STEP/2, plot_x2 - PLOT_STEP/2])
+    x = torch.cat([x1.flatten().unsqueeze(dim = 1), x2.flatten().unsqueeze(dim = 1)], dim = 1)
+    plot_yhat = h.forward(x).reshape([plot_x1.size()[0], plot_x2.size()[0]])
+
+    ax.pcolormesh(x1, x2, plot_yhat, cmap = ListedColormap(COLORS[0:h.K]), alpha = 0.5, shading = 'auto')
+
+    plt.show()
