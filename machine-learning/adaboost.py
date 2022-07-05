7d0b201bff684309d9b73e1cdb8f5d70878a4df1
votrinhan88 <votrinhan88@gmail.com>
2022-07-06 01:13:48 +0700

    AdaBoost: rewrite DecisionTreeAdaBoost (to compute with weights), finish algorithm, sanity check with sklearn

--- machine-learning/adaboost.py
+++ machine-learning/adaboost.py
@@ -1,10 +1,10 @@
-# Topic: Supervised, Classification, Ensemble, Boosting
+# Topic: Supervised > Classification, Ensemble > Boosting
+
 import torch
 import torch.nn.functional as F
-from typing import Literal, Union, Tuple
+from typing import Literal, Tuple
 
 from decision_tree import DecisionTree, Node
-
 
 class DecisionTreeAdaBoost(DecisionTree):
     def __init__(self, method_info:Literal['gini', 'entropy'] = 'gini',
@@ -34,21 +34,14 @@ class DecisionTreeAdaBoost(DecisionTree):
         self.grow(self.root, idx_root)
 
     def grow(self, node:Node, idx_node:torch.Tensor):
-        X_node = self.X_train[idx_node]
         max_gain = self.find_best_split(node, idx_node)
         if max_gain > 0:
             # Continue growing
-            ## Re-split data by optimal feature and threshold for children nodes
             self.depth = max(self.depth, node.depth + 1)
             left_node, right_node = node.branch()
             node.children = [left_node, right_node]
-
-            left_ind = X_node[:, node.feature] <= node.threshold
-            idx_left = idx_node[left_ind]
-            idx_right = idx_node[~left_ind]
-            # X_left, y_left = X_node[left_ind], y_node[left_ind]
-            # X_right, y_right = X_node[~left_ind], y_node[~left_ind]
-            
+            # Re-split data by optimal feature and threshold for children nodes
+            idx_left, idx_right = self.split(idx_node, feature = node.feature, threshold = node.threshold)
             # Leaf node:
             #  - Has a unique class distribution (contains only one class)
             #  - OR has reached max depth
@@ -98,26 +91,35 @@ class DecisionTreeAdaBoost(DecisionTree):
     def compute_gain(self, node:Node, idx_node:torch.Tensor, idx_left:torch.Tensor, idx_right:torch.Tensor) -> torch.Tensor:
         left_info = self.compute_info(idx_left)
         right_info = self.compute_info(idx_right)
-        gain = node.info - (idx_left.size()[0]*left_info + idx_right.size()[0]*right_info)/idx_node.size()[0]
+        # gain = node.info - (idx_left.size()[0]*left_info + idx_right.size()[0]*right_info)/idx_node.size()[0]
+        w_node = self.weights[idx_node].sum()
+        w_left = self.weights[idx_left].sum()
+        w_right = self.weights[idx_right].sum()
+        gain = w_node*node.info - (w_left/w_node*left_info + w_right/w_node*right_info)
         return gain
 
     def feed_c(self, node:Node, idx_label:torch.Tensor):
         label = self.y_train[idx_label]
-        node.distr:torch.Tensor = F.one_hot(label.squeeze(dim = 1), num_classes = self.num_classes).sum(dim = 0)
-        node.value = node.distr.max(dim = 0)[1]
         node.info = self.compute_info_c(idx_label)
+        
+        # node.distr:torch.Tensor = F.one_hot(label.squeeze(dim = 1), num_classes = self.num_classes).sum(dim = 0)
+        onehot:torch.Tensor = F.one_hot(label.squeeze(dim = 1), num_classes = self.num_classes)
+        node.distr = (self.weights[idx_label]*onehot).sum(dim = 0)
+        node.distr = node.distr/node.distr.sum()
+        
+        node.value = node.distr.max(dim = 0)[1]
 
     def compute_info_c(self, idx_label:torch.Tensor) -> torch.Tensor:
         label = self.y_train[idx_label]
         weights = self.weights[idx_label]
         
         # Classification: reducing Gini impurity or entropy
-        distr:torch.Tensor = F.one_hot(label.squeeze(dim = 1), num_classes = self.num_classes)
+        onehot:torch.Tensor = F.one_hot(label.squeeze(dim = 1), num_classes = self.num_classes)
         # Weighted distribution
-        distr = (weights*distr).sum(dim = 0)
-        distr = distr/distr.sum()
+        weighted_distr = (weights*onehot).sum(dim = 0)
+        weighted_distr = weighted_distr/weighted_distr.sum()
         if self.method_info == 'gini':
-            info = 1 - (distr**2).sum()
+            info = 1 - (weighted_distr**2).sum()
         elif self.method_info == 'entropy':
             # Side note:
             # 1. Entropy of a system (all classes) = sum of entropy of its parts (each class)
@@ -125,15 +127,19 @@ class DecisionTreeAdaBoost(DecisionTree):
             #   entropy is 0.
             # 3. To ease dealing with absent classes (which yields log(0) = -inf), and (2.), the 
             #   computation only considers existing classes.
-            info = -(distr[distr != 0]*distr[distr != 0].log()).sum()
+            info = -(weighted_distr[weighted_distr != 0]*torch.log(weighted_distr[weighted_distr != 0])).sum()
         return info   
 
 class AdaBoost():
-    EPSILON = torch.finfo(torch.float).eps
-    def __init__(self, method_info:Literal['gini', 'entropy'] = 'gini', num_learners:int = 100, max_depth:int = 1, learning_rate:float = 0.5):
+    def __init__(self, method_info:Literal['gini', 'entropy'] = 'gini',
+                 num_learners:int = 20,
+                 max_depth:int = 1,
+                 drop_features:bool = True,
+                 learning_rate:float = 0.5):
         self.method_info = method_info
         self.num_learners = num_learners
         self.max_depth = max_depth
+        self.drop_features = drop_features
         self.learning_rate = learning_rate
 
     def __repr__(self) -> str:
@@ -142,13 +148,14 @@ class AdaBoost():
     def fit(self, X_train:torch.Tensor, y_train:torch.Tensor):
         self.num_classes = len(y_train.unique())
         num_examples = X_train.size()[0]
+        self.EPSILON = 1e-7/num_examples
         # Init
-        self.learners = [DecisionTreeAdaBoost(method_info = self.method_info,       # 
+        self.learners = [DecisionTreeAdaBoost(method_info = self.method_info,
                                               max_depth   = self.max_depth,
-                                              drop_features = False)
+                                              drop_features = True)
                         for learner in range(self.num_learners)]
         self.weights = torch.ones([num_examples, 1])/num_examples
-        self.alpha = torch.zeros([self.num_learners])  # Weight of learners
+        self.alpha = torch.zeros([self.num_learners])               # Weight of learners
         self.num_cur_learners = 0
         for learner_idx, learner in enumerate(self.learners):
             # Fit weak-learner and predict
@@ -156,12 +163,14 @@ class AdaBoost():
             yhat = learner.forward(X_train)
             incorrects = (yhat != y_train)
 
-            error = (self.weights*incorrects).sum()/num_examples
+            error = self.weights[incorrects].sum()/num_examples
             alpha = 0.5*torch.log((1 - error)/(error + self.EPSILON))
             
             # Adjust examples' weight (favoring wrong predictions)
             self.weights[incorrects]  = self.weights[incorrects] *torch.exp(alpha*self.learning_rate)
             self.weights[~incorrects] = self.weights[~incorrects]*torch.exp(-alpha*self.learning_rate)
+            # Set lower boundary for very small weights for stability
+            self.weights[self.weights < self.EPSILON] = self.EPSILON
             self.weights = self.weights/self.weights.sum()
             # Save weak-learner's weight
             self.alpha[learner_idx] = alpha
@@ -169,17 +178,15 @@ class AdaBoost():
             self.num_cur_learners = self.num_cur_learners + 1
 
     def strong_learner(self, input:torch.Tensor):
-        pred_learners = torch.zeros([input.size()[0], self.num_cur_learners], dtype = torch.long)
+        yhat = torch.zeros([input.size()[0], 1], dtype = torch.float)
         for l_idx in torch.arange(self.num_cur_learners):
             pred_learner = self.learners[l_idx].forward(input)
             pred_learner[pred_learner == 0] = -1
-            pred_learners[:, [l_idx]] = pred_learner
+            # Apply weights to each learners
+            yhat = yhat + self.alpha[l_idx] * pred_learner
         
-        # Apply weights to each learners
-        pred_learners = self.alpha * pred_learners
-
         # Strong learner's prediction is based on the sign of sum of weighted predictions from weak-learners
-        yhat = (pred_learners.sum(dim = 1, keepdim = True) >= 0).type(torch.long)
+        yhat = (yhat >= 0).type(torch.long)
         return yhat
 
     def forward(self, input:torch.Tensor):
@@ -188,46 +195,75 @@ class AdaBoost():
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from matplotlib.colors import ListedColormap
+    from sklearn.ensemble import AdaBoostClassifier
 
     from utils_data import get_clusters_2D
 
+    # Dummy data
     NUM_CLUSTERS = 2
-    PLOT_STEP = 0.05
+    SIGMA_DIAG = 0.5
+    NUM_EXAMPLES = 200
+    # Model
+    METHOD_INFO = 'gini'
+    NUM_LEARNERS = 15
+    MAX_DEPTH = 1
+    LEARNING_RATE = 0.5
+    DROP_FEATURES = False
+    # Visual
+    PLOT_STEP = 0.01
     COLORS = ['red', 'blue', 'green', 'magenta', 'cyan', 'yellow', 'black', 'tab:blue', 'tab:orange', 'tab:green',
             'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
 
-    X_train, y_train = get_clusters_2D(num_clusters = NUM_CLUSTERS, sigma_diag = 0.7, num_examples = 200)
+    # Dummy data (slightly swirl pattern with noise in the center)
+    X_train, y_train = get_clusters_2D(num_clusters = NUM_CLUSTERS,
+                                       sigma_diag = SIGMA_DIAG,
+                                       num_examples = NUM_EXAMPLES)
     y_train[(X_train[:, 0] < 1) & (X_train[:, 1] > 0.5), :] = 0
     y_train[(X_train[:, 0] >-1) & (X_train[:, 1] < -0.5), :] = 1
-    # y_train[y_train == 0] = -1
 
-    h = AdaBoost(num_learners = 10, max_depth = 1, learning_rate = 0.2)
-    h.fit(X_train, y_train)
-
-    yhat = h.forward(X_train)
+    # Model & Fit (also from sklearn for sanity check)
+    h = AdaBoost(method_info = METHOD_INFO,
+                 num_learners = NUM_LEARNERS, 
+                 max_depth = MAX_DEPTH,
+                 learning_rate = LEARNING_RATE,
+                 drop_features = DROP_FEATURES)
+    # h = AdaBoostClassifier(n_estimators = NUM_LEARNERS, learning_rate = LEARNING_RATE, algorithm = 'SAMME')
+    if h.__class__ == AdaBoost:
+        h.fit(X_train, y_train)
+        yhat = h.forward(X_train)
+    elif h.__class__ == AdaBoostClassifier:
+        h.num_classes = X_train.size()[1]
+        h.forward = h.predict
+        h.fit(X_train, y_train)
+        yhat = torch.tensor(h.forward(X_train)).unsqueeze(dim = 1)
     print(f'Train accuracy: {((yhat == y_train).sum()/y_train.size()[0]).item():.4f}')
 
     # Visualize regions of each class
     ptp_X = X_train.max(dim = 0)[0] - X_train.min(dim = 0)[0]
     plot_x1 = torch.arange(X_train[:, 0].min() - 0.2*ptp_X[0], X_train[:, 0].max() + 0.2*ptp_X[1], PLOT_STEP)
     plot_x2 = torch.arange(X_train[:, 1].min() - 0.2*ptp_X[0], X_train[:, 1].max() + 0.2*ptp_X[1], PLOT_STEP)
-    x1, x2 = torch.meshgrid([plot_x1 - PLOT_STEP/2, plot_x2 - PLOT_STEP/2])
+    x1, x2 = torch.meshgrid([plot_x1, plot_x2])
     X_plot = torch.cat([x1.flatten().unsqueeze(dim = 1), x2.flatten().unsqueeze(dim = 1)], dim = 1)
-    yhat_plot = h.forward(X_plot).reshape([plot_x1.size()[0], plot_x2.size()[0]])
+    # yhat_plot = h.forward(X_plot).reshape([plot_x1.size()[0], plot_x2.size()[0]])
+    yhat_plot = torch.tensor(h.forward(X_plot)).reshape([plot_x1.size()[0], plot_x2.size()[0]])
     
     # Plot all examples
     fig, ax = plt.subplots()
-    ax.set_title(f'{h} for {NUM_CLUSTERS} clusters')
-    for label in torch.arange(h.num_classes):
+    ax.set_title(f'{h} for {h.num_classes} clusters')
+    for label in torch.arange(NUM_CLUSTERS):
         idx_label = (y_train.squeeze() == label)
         # Training data
-        ax.scatter(X_train[idx_label, 0], X_train[idx_label, 1],
-                   s = torch.min(h.weights[idx_label]/h.weights.min(), torch.tensor([200])),
-                   color = COLORS[label], zorder = 100)
+        if h.__class__ == AdaBoostClassifier:
+            ax.scatter(X_train[idx_label, 0], X_train[idx_label, 1],
+                       color = COLORS[label], zorder = 100)
+        elif h.__class__ == AdaBoost:
+            s = (h.weights[idx_label].log() - h.weights.min().log() + 3)*(10/(h.weights.max().log() - h.weights.min().log() + 3))
+            s = s**2
+            ax.scatter(X_train[idx_label, 0], X_train[idx_label, 1],
+                       s = s, color = COLORS[label], zorder = 100)
 
     ax.pcolormesh(x1, x2, yhat_plot, cmap = ListedColormap(COLORS[0:h.num_classes]), alpha = 0.5, shading = 'auto')
 
     plt.show()
 
     print()
-
